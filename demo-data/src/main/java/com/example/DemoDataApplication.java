77c601d4c99a25a676d1132426b488293b8ee85b
Javier <jmartin@grupoloyal.com>
2024-10-31 12:18:50 +0100

    Consultas

--- demo-data/src/main/java/com/example/DemoDataApplication.java
+++ demo-data/src/main/java/com/example/DemoDataApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 import com.example.domains.entities.Veterinario;
 import com.example.domains.repositories.VeterinariosRepository;
@@ -20,8 +21,8 @@ public class DemoDataApplication implements CommandLineRunner {
 		System.err.println("Aplicación arrancada");
 	}
 
-	@Bean
-	CommandLineRunner datos(VeterinariosRepository dao) {
+//	@Bean
+	CommandLineRunner crud(VeterinariosRepository dao) {
 		return (args) -> {
 			var id = dao.save(new Veterinario("Pepito", "Grillo")).getId();
 			dao.findAll().forEach(System.out::println);
@@ -38,6 +39,22 @@ public class DemoDataApplication implements CommandLineRunner {
 			System.out.println("---------------------------------------");
 			dao.deleteById(id);
 			dao.findAll().forEach(System.out::println);
+		};
+	}
+//	@Bean
+	CommandLineRunner consultas(VeterinariosRepository dao) {
+		return (args) -> {
+			dao.findTop3ByFirstNameContainingOrderByLastNameDesc("e").forEach(System.out::println);
+			System.out.println("---------------------------------------");
+			dao.findByFirstNameContaining("e", Sort.by("FirstName").descending()).forEach(System.out::println);
+			System.out.println("---------------------------------------");
+			dao.findByIdGreaterThan(4).forEach(System.out::println);
+			System.out.println("---------------------------------------");
+			dao.findNuevosJPQL(4).forEach(System.out::println);
+			System.out.println("---------------------------------------");
+			dao.findNuevosSQL(4).forEach(System.out::println);
+			System.out.println("---------------------------------------");
+			dao.findAll((root, query, builder) -> builder.greaterThan(root.get("id"), 4)).forEach(System.out::println);
 		};
 	}
 }
--- demo-data/src/main/java/com/example/domains/repositories/VeterinariosRepository.java
+++ demo-data/src/main/java/com/example/domains/repositories/VeterinariosRepository.java
@@ -1,9 +1,23 @@
 package com.example.domains.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.domains.entities.Veterinario;
 
-public interface VeterinariosRepository extends JpaRepository<Veterinario, Integer> {
-
+public interface VeterinariosRepository extends JpaRepository<Veterinario, Integer>, JpaSpecificationExecutor<Veterinario> {
+	List<Veterinario> findTop3ByFirstNameContainingOrderByLastNameDesc(String unaLetra);
+	List<Veterinario> findByFirstNameContaining(String unaLetra, Sort orderBy);
+	
+	List<Veterinario> findByIdGreaterThan(int id);
+	@Query(value = "from Veterinario v where v.id > ?1")
+	List<Veterinario> findNuevosJPQL(int id);
+	@Query(value = "select * from vets v where v.id > :id", nativeQuery = true)
+	List<Veterinario> findNuevosSQL(int id);
+	
+	
 }
